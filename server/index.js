f442b3e92bcf6359fef9f42263e728be1699a1ef
nidhi patel <nidhi@ealphabits.com>
2023-02-09 16:28:37 +0530

    production modified

--- server/index.js
+++ server/index.js
@@ -7,17 +7,26 @@ const path = require('path');
 
 let socketList = {};
 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/build/index.html')),
-    function(err){
-      if(err){
-        res.status(500).send(err);
-      }
-    }
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
+}
+
+// app.use(express.static(path.join(__dirname, '../client/build')));
+
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../client/build/index.html')),
+//     function(err){
+//       if(err){
+//         res.status(500).send(err);
+//       }
+//     }
+//   });
 
 
 // Route
